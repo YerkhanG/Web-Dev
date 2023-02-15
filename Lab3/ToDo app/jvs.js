e6c331883acf135e7e608548d7559a9010ce8349
Yerkhan <sarshaevyerkhan@gmail.com>
2023-02-15 16:40:30 +0600

    test

---
+++ Lab3/ToDo app/jvs.js
@@ -0,0 +1,51 @@
+let createNewTask = function(taskString) {
+
+    let nTask = document.createElement("li");
+    nTask.className = "task";
+
+
+    let box = document.createElement("input");
+    box.className = "c-boxes";
+    box.type = "checkbox";
+    
+    let deleteButton = document.createElement("img");
+    deleteButton.src = "https://cdn-icons-png.flaticon.com/512/860/860829.png";
+    deleteButton.className = "delete";
+    deleteButton.onclick = deleteItem;
+
+
+    
+    let span = document.createElement("span");
+    span.className = "span";
+
+    
+
+    span.innerHTML = taskString;
+    nTask.appendChild(box);
+    nTask.appendChild(span);
+    nTask.appendChild(deleteButton);
+
+    return nTask
+}
+
+let deleteItem = function() {
+    
+    let listItem = this.parentNode;
+    listItem.remove();
+    
+}
+
+
+let addItem = function() {
+    
+    let tasks = document.getElementById("task");
+    let task = document.getElementById("main");
+    if(task.value === "") {
+        alert("Write something!")
+    } else {
+        let Item = createNewTask(task.value);
+        tasks.appendChild(Item);
+    }
+    task.value = "";
+}
+
