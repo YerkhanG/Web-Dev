d613bbff1622fc40601fda3806519511ceb68f67
Yerkhan <sarshaevyerkhan@gmail.com>
2023-02-22 00:13:02 +0600

    test

--- MyApp/src/app/app.component.spec.ts
+++ MyApp/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MyApp'`, () => {
+  it(`should have as title 'shop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyApp');
+    expect(app.title).toEqual('shop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MyApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('shop app is running!');
   });
 });
